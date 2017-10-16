4037d7c17d3409f9bc644b6797b3888c26fb9808
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-16 22:46:39 +0300

    Added methods for searching group names and Schedule

--- app/src/main/java/com/example/nickolas/kpiweeks/api/KpiApi.kt
+++ app/src/main/java/com/example/nickolas/kpiweeks/api/KpiApi.kt
@@ -1,10 +1,19 @@
 package com.example.nickolas.kpiweeks.api
 
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import rx.Observable
+
 /**
  * Created by Nickolas on 15.10.2017.
  */
 interface KpiApi {
 
+    @GET("groups.json")
+    fun searchGroup(@Query("search") str: String) : Observable<ResponseBody>
 
-
+    @GET("groups/{group}/timetable")
+    fun getTimetable(@Path("group") group : String) : Observable<ResponseBody>
 }
