24c7c3ba60fa95870b7e5e08f9f8dba0e48a443a
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-16 22:55:58 +0300

    Added presenter for week

---
+++ app/src/main/java/com/example/nickolas/kpiweeks/presenters/WeekPresenter.kt
@@ -0,0 +1,22 @@
+package com.example.nickolas.kpiweeks.presenters
+
+import com.example.nickolas.kpiweeks.model.enteties.Week
+import com.example.nickolas.kpiweeks.model.remote.IWeekDataSource
+import com.example.nickolas.kpiweeks.utils.ResponseToSchedule
+import com.example.nickolas.kpiweeks.utils.rx.RxRetryWithDelay
+import com.example.nickolas.kpiweeks.views.WeekView
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+
+class WeekPresenter(val source: IWeekDataSource) : BasePresenter<WeekView>() {
+    fun getSchedule(group: String) {
+        subscribe(source.getSchedule(group)
+                .retryWhen(RxRetryWithDelay())
+                .subscribeOn(Schedulers.io())
+                .map<Week>{ResponseToSchedule.parse(it)}
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({view?.showSchedule(it)}))
+    }
+
+}
