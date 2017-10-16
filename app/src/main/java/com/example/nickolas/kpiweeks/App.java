e8a848910e6e8aae4191d7773be422817b2cd5ac
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-16 22:52:33 +0300

    Added constants and app classes

---
+++ app/src/main/java/com/example/nickolas/kpiweeks/App.java
@@ -0,0 +1,29 @@
+package com.example.nickolas.kpiweeks;
+
+
+import android.app.Application;
+
+import com.example.nickolas.kpiweeks.di.component.AppComponent;
+//import com.example.nickolas.kpiweeks.di.component.DaggerAppComponent;
+//import com.example.nickolas.kpiweeks.di.component.DaggerAppComponent;
+import com.example.nickolas.kpiweeks.di.component.DaggerAppComponent;
+import com.example.nickolas.kpiweeks.di.module.ApiModule;
+import com.example.nickolas.kpiweeks.di.module.AppModule;
+
+public class App extends Application {
+
+    private AppComponent appComponent;
+
+    public App() {
+        super();
+
+        appComponent = DaggerAppComponent.builder()
+                .apiModule(new ApiModule())
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent appComponent() {
+        return appComponent;
+    }
+}
---
+++ app/src/main/java/com/example/nickolas/kpiweeks/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.nickolas.kpiweeks
+
+
+object Constants {
+    var BASE_URL = "http://api.rozklad.hub.kpi.ua"
+}
