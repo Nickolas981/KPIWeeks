5d9813f24412282c00a964c0d1faffd7a5d7e090
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-16 22:55:46 +0300

    Added presenter for search

---
+++ app/src/main/java/com/example/nickolas/kpiweeks/presenters/SearchPresenter.kt
@@ -0,0 +1,26 @@
+package com.example.nickolas.kpiweeks.presenters
+
+import com.example.nickolas.kpiweeks.model.remote.ISearchDataSource
+import com.example.nickolas.kpiweeks.model.remote.SearchDataSource
+import com.example.nickolas.kpiweeks.utils.ResponseToGroupList
+import com.example.nickolas.kpiweeks.utils.rx.RxRetryWithDelay
+import com.example.nickolas.kpiweeks.views.GroupSearchView
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+/**
+ * Created by Nickolas on 15.10.2017.
+ */
+class SearchPresenter(val source: ISearchDataSource) : BasePresenter<GroupSearchView>() {
+    fun getGroups(str: String) {
+
+        subscribe(source.getGroups(str)
+                .retryWhen(RxRetryWithDelay())
+                .map<List<String>> { ResponseToGroupList.parse(it) }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ view?.showPredictions(it) })
+        )
+
+    }
+}
