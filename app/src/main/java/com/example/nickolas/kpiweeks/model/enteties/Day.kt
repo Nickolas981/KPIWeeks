2454cca4323097de9d3e18e5794d16702fcf63f9
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-31 20:03:48 +0300

    Added Inheritance Item() for Sticky headers

--- app/src/main/java/com/example/nickolas/kpiweeks/model/enteties/Day.kt
+++ app/src/main/java/com/example/nickolas/kpiweeks/model/enteties/Day.kt
@@ -5,11 +5,11 @@ import com.google.gson.annotations.SerializedName
 data class Day(
 
         @field:SerializedName("day_name")
-        val dayName: String? = null,
+        var dayName: String? = null,
 
         @field:SerializedName("day_number")
         val dayNumber: Int? = null,
 
         @field:SerializedName("lessons")
         val lessons: List<Lesson?>? = null
-)
+) : Item()
