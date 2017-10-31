44bd368bf014eae77007d14d355797c8d7543a6d
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-31 21:17:32 +0300

    Removed empty days

--- app/src/main/java/com/example/nickolas/kpiweeks/widgets/adapters/WeekAdapter.kt
+++ app/src/main/java/com/example/nickolas/kpiweeks/widgets/adapters/WeekAdapter.kt
@@ -24,7 +24,7 @@ import kotlinx.android.synthetic.main.lessons_list_view.view.*
 open class WeekAdapter(private val context: Context) : RecyclerView.Adapter<WeekAdapter.BaseViewHolder>(), StickyHeaderHandler {
 
 
-    public var week = Week()
+    var week = Week()
         set(w) {
             val diffResult = DiffUtil.calculateDiff(SimpleDiffCallback(list, w.getAsList()))
             field = w
@@ -32,7 +32,6 @@ open class WeekAdapter(private val context: Context) : RecyclerView.Adapter<Week
             diffResult.dispatchUpdatesTo(this)
         }
     private var list: MutableList<Item> = ArrayList()
-
 
 
     override fun getAdapterData(): MutableList<*> = list
@@ -45,8 +44,11 @@ open class WeekAdapter(private val context: Context) : RecyclerView.Adapter<Week
         if (getItemViewType(position) == 1) {
             item as HeaderItem
             val header = holder as HeaderHolder
-            header.header.text = item.text
-        } else {
+            if ((list[position + 1] as Day).lessons?.isNotEmpty()!!) {
+                header.header.text = item.text
+                header.header.visibility = View.VISIBLE
+            }
+        } else if(getItemViewType(position) == 2){
             val day = item as Day
             val hold = holder as LessonHolder
             val vi = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -62,28 +64,39 @@ open class WeekAdapter(private val context: Context) : RecyclerView.Adapter<Week
                     v.teacher_name.text = lesson?.teacherName
                     v.room_number.text = "Аудиторія: ${lesson?.lessonRoom}"
                     val date = day.dayName?.replace("[^0-9.]".toRegex(), "")
-                    if (dayInformationUtil.isActive(lesson?.lessonNumber.toString(), date!!)){
+                    if (dayInformationUtil.isActive(lesson?.lessonNumber.toString(), date!!)) {
                         v.indicator.visibility = View.VISIBLE
                     }
-                    v.lesson_start.text = lesson?.timeStart
-                    v.lesson_end.text = lesson?.timeEnd
+                    v.lesson_start.text = lesson?.timeStart?.substring(5)
+                    v.lesson_end.text = lesson?.timeEnd?.substring(5)
                     container.addView(v)
                 }
             }
         }
     }
 
-    override fun getItemViewType(position: Int): Int = if (position % 2 == 0) 1 else 2
+    override fun getItemViewType(position: Int): Int {
+        if (position % 2 == 0){
+            val day = list[position + 1] as Day
+            if (day.lessons?.isEmpty()!!)
+                return 3
+            return 1
+        }
+        return 2
+
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseViewHolder {
         val view = LayoutInflater.from(parent?.context)
                 .inflate(
-                        if (viewType == 1)
-                            R.layout.day_view
-                        else
-                            R.layout.lessons_list_view
+                        when (viewType) {
+                            1 -> R.layout.day_view
+                            2 -> R.layout.lessons_list_view
+                            else -> R.layout.empty_view
+                        }
                         , parent, false)
+
 
         return (if (viewType == 1)
             HeaderHolder(view)
