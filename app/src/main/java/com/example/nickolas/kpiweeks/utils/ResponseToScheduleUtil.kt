69196f1e2e7e241672c66f763452b1ad28b8813f
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-31 20:02:03 +0300

    Added dates in dayName property

--- app/src/main/java/com/example/nickolas/kpiweeks/utils/ResponseToScheduleUtil.kt
+++ app/src/main/java/com/example/nickolas/kpiweeks/utils/ResponseToScheduleUtil.kt
@@ -10,60 +10,19 @@ import okhttp3.ResponseBody
 class ResponseToScheduleUtil {
     companion object {
 
-
-//        fun parse(response: ResponseBody): MutableList<Week> {
-//            var list: MutableList<Week> = ArrayList()
-//            val typeOfHashMap = object : TypeToken<MutableList<Week>>() {}.type
-//            val str = response.string()
-//
-//            if (!str.contains("data"))
-//                list = GsonBuilder().create().fromJson(str, typeOfHashMap)
-//            else {
-//                val json = JsonParser().parse(str)
-//                        .asJsonObject
-//                        .getAsJsonObject("data")
-//
-//
-//                list.add(parse_(json, 1))
-//                list.add(parse_(json, 2))
-//
-//                val resultInJson = GsonBuilder().create().toJson(list)
-//                ScheduleActivity.sharedPreferenceUtils.setValue("json", resultInJson)
-//
-//            }
-//
-//            return list
-//        }
-
-        //        private fun parse_(jsonObject: JsonObject, w: Int): Week {
-//            val week = Week()
-//
-//            val weekJson = jsonObject.getAsJsonObject(w.toString())
-//
-//            val dayKeys = weekJson.keySet()
-//            for (dayKey in dayKeys) {
-//                val dayJson = weekJson.getAsJsonObject(dayKey)
-//                val day = Day()
-//                val lessonKeys = dayJson.keySet()
-//                for (lessonKey: String in lessonKeys) {
-//                    val lesson = Gson().fromJson<Lesson>(dayJson.get(lessonKey) as JsonElement,
-//                            Lesson::class.java)
-//
-//                    day.lesssons[lessonKey] = lesson
-//                }
-//                week.days[dayKey] = day
-//            }
-//            return week
-//        }
-
         private fun parseWeek(js: JsonObject): Week {
             val week = Week()
-
             week.weekNumber = js.get("week_number").asString
+            val dayInfo = DayInformationUtil()
+            val dates = dayInfo.getDates(week.weekNumber.toInt() - 1)
+
             val json = js.getAsJsonObject("days")
             for (i in 1..6) {
-                week.addDay(Gson().fromJson<Day>
-                ( json.getAsJsonObject(i.toString()) as JsonElement, Day::class.java))
+                val day = Gson()
+                        .fromJson<Day>(json.getAsJsonObject(i.toString()) as JsonElement,
+                                Day::class.java)
+                day.dayName += " " + dates[i - 1]
+                week.addDay(day)
             }
             return week
         }
