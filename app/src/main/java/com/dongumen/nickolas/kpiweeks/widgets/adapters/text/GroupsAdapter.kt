a67b9fe863af3a6665b125b01875a59c73962eff
Nickolas9811 <nickolasgumeniuk@gmail.com>
2018-09-07 19:06:21 +0300

    Created groups adapter

---
+++ app/src/main/java/com/dongumen/nickolas/kpiweeks/widgets/adapters/text/GroupsAdapter.kt
@@ -0,0 +1,24 @@
+package com.dongumen.nickolas.kpiweeks.widgets.adapters.text
+
+import com.dongumen.nickolas.kpiweeks.R
+import com.dongumen.nickolas.kpiweeks.model.enteties.groups.Group
+import com.dongumen.nickolas.kpiweeks.widgets.adapters.delegateAdapter.DelegateAdapter
+import com.mcxiaoke.koi.ext.onClick
+import kotlinx.android.synthetic.main.list_item_text.*
+
+class GroupsAdapter(private val onClick: (Group) -> Unit) : DelegateAdapter<Group>() {
+    override fun onBind(item: Group, viewHolder: KViewHolder) {
+        with(viewHolder) {
+            text.text = item.name
+            viewHolder.itemView.onClick { onClick.invoke(item) }
+        }
+    }
+
+    override fun getLayoutId(): Int {
+        return R.layout.list_item_text
+    }
+
+    override fun isForViewType(item: Group?): Boolean {
+        return true
+    }
+}
