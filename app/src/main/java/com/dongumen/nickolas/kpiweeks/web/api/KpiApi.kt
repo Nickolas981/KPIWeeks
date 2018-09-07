28a640f22996d3b56100ede200440fa56bc34b99
Nickolas9811 <nickolasgumeniuk@gmail.com>
2018-09-07 19:09:22 +0300

    Changed searchGroup method

---
+++ app/src/main/java/com/dongumen/nickolas/kpiweeks/web/api/KpiApi.kt
@@ -0,0 +1,22 @@
+package com.dongumen.nickolas.kpiweeks.web.api
+
+import com.dongumen.nickolas.kpiweeks.model.enteties.groups.GroupResponse
+import com.dongumen.nickolas.kpiweeks.web.GROUPS
+import com.dongumen.nickolas.kpiweeks.web.TIMETABLE
+import kotlinx.coroutines.experimental.Deferred
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import rx.Observable
+
+interface KpiApi {
+    @GET(GROUPS)
+    fun searchGroup(@Query("search") search: String,
+                    @Query("limit") limit: Int = 30)
+            : Deferred<Response<GroupResponse>>
+
+    @GET(TIMETABLE)
+    fun getTimetable(@Path("group") group: String): Observable<ResponseBody>
+}
