24f892ce19888c358fb2ef8befd497021f8b9fef
Nickolas9811 <nickolasgumeniuk@gmail.com>
2018-09-07 19:05:27 +0300

    Changed base url

--- app/src/main/java/com/dongumen/nickolas/kpiweeks/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.dongumen.nickolas.kpiweeks
-
-
-object Constants {
-    var BASE_URL = "http://api.rozklad.org.ua/v2/"
-}
---
+++ app/src/main/java/com/dongumen/nickolas/kpiweeks/web/Endpoints.kt
@@ -0,0 +1,5 @@
+package com.dongumen.nickolas.kpiweeks.web
+
+const val BASE_URL = "http://api.rozklad.hub.kpi.ua/"
+const val GROUPS = "groups/"
+const val TIMETABLE = "groups/{group}/timetable"
